b4362e78d2bcabcf3547002687745704f9ce5fc4
KangBit <kangbit@inbm.co.kr>
2019-07-25 17:53:29 +0900

    -

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan('dev', { stream: logger.stream }));
 
-app.use('/user', userRouter);
+// app.use('/user', userRouter);
 
 // Index route
 app.get('/', (req, res) => {
