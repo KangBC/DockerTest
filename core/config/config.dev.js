6291fe4550ccceff0f64805a28d1a2b368954f44
KangBit <kangbit@inbm.co.kr>
2019-07-26 09:52:03 +0900

    change port num

--- core/config/config.dev.js
+++ core/config/config.dev.js
@@ -7,7 +7,7 @@ config.logFileName = 'app.log';
 config.dbHost = process.env.dbHost || 'localhost';
 config.dbPort = process.env.dbPort || '27017';
 config.dbName = process.env.dbName || 'fnsapi';
-config.serverPort = process.env.serverPort || 80;
+config.serverPort = process.env.serverPort || 3256;
 config.TOKEN_SECRET = 'weroiusdf34werljk90123sdlfkjsr';
 
 export default config;
