8b3eae90d4a29b5b7dadb056693ab3f8a0f7fad4
KangBit <kangbit@inbm.co.kr>
2019-07-26 09:37:04 +0900

    --

--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,9 @@
 import express from 'express';
-import User from '../models/Users';
-import bcrypt from 'bcryptjs';
-import jwt from 'jsonwebtoken';
-import config from '../core/config/config.dev';
-import verifyToken from '../modules/verifyToken';
-import { AddUserValidation, LoginValidation } from '../models/Validations';
 
 const router = express.Router();
 
 // User Info
-router.get('/', verifyToken, (req, res) => {
+router.get('/1', verifyToken, (req, res) => {
   res.send('user!!');
 });
 
